81f054733f928af9f3283e103862a9654bc3e0f6
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-08-09 15:35:55 +0300

    update

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { GlobalStyles } from './GlobalStyle';
+
+export const App = () => {
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import { App } from 'components/App';
+import { GlobalStyles } from './GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <GlobalStyles />
   </React.StrictMode>
 );
