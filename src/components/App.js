7c254c7e4a0dfc6b9184817fd26d500d9dc6ce2e
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-08-10 11:16:18 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 import { Feedback } from './Feedback/Feedback';
 import { GlobalStyles } from './GlobalStyle';
 import { Statistics } from './Statistics/Statistics';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-      <Statistics />
-      <GlobalStyles />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    goodNumbOfClicks: 0,
+    neutralNumbOfClicks: 0,
+    badNumbOfClicks: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <Feedback
+          goodClicks={this.state.goodNumbOfClicks}
+          neutralClicks={this.state.neutralNumbOfClicks}
+          badClicks={this.state.badNumbOfClicks}
+        />
+        <Statistics
+          goodClicks={this.state.goodNumbOfClicks}
+          neutralClicks={this.state.neutralNumbOfClicks}
+          badClicks={this.state.badNumbOfClicks}
+        />
+        <GlobalStyles />
+      </div>
+    );
+  }
+}
