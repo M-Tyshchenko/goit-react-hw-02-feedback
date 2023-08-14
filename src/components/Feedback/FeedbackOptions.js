9ac6d1f4cd58984b38606140961efa0dc2f308d0
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-08-14 14:37:02 +0300

    Update FeedbackOptions.js

--- src/components/Feedback/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions.js
@@ -1,10 +1,14 @@
 import { BtnContainer, BtnItem } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <BtnContainer>
-    {options.map(option => (
-      <BtnItem onClick={onLeaveFeedback(option.key)}>{option.key}</BtnItem>      
+      {options.map(option => (        
+        <BtnItem onClick={onLeaveFeedback(option)}>{option}</BtnItem>
+    
+        )
+      )
+    }
     </BtnContainer>
   );
-};
+}
